467666f0df6ab27b06fd656f29c75b545e473385
William Kunitake <william@bulkdesign.com.br>
2025-06-07 11:32:10 -0700

    Re-structured HTML

--- one-flat-fee-package-calculator.php
+++ one-flat-fee-package-calculator.php
@@ -23,33 +23,12 @@ function one_flat_fee_package_calculator() { ?>
     <?php ob_start(); ?>
 
     <div class="one-flat-fee-package-calculator">
-        <h1>One Flat Fee Package Calculator</h1>
-        <p>Estimate Your Monthly Payment for a 35-Year Mortgage:</p>
+        <h1>Package Calculator</h1>
+        <p>FULL SERVICE DISCOUNT REALTOR®</p>
         <form>
-            <div class="mortgage">
-                <label><strong>Mortgage Amount ($):</strong></label>
+            <div class="selling-price">
+                <label><strong>Selling Price ($):</strong></label>
                 <input type="text" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');" placeholder="$CAD" required>
-            </div>
-
-            <div class="down-payment">
-                <label><strong>Down Payment ($):</strong></label>
-                <input type="text" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');" placeholder="$CAD" required>
-            </div>
-
-            <div class="interest">
-                <label><strong>Interest Rate (%):</strong></label>
-                <input type="text" oninput="this.value = this.value.replace(/[^0-9.]/g, '').replace(/(\..*?)\..*/g, '$1');" placeholder="E.g. 3.5%" required>
-            </div>
-
-            <div class="loan-term">
-                <label><strong>Select Length of Term:</strong></label>
-                <select class="loanTerm" required>
-                    <option value="15">15 years</option>
-                    <option value="20">20 years</option>
-                    <option value="25">25 years</option>
-                    <option value="30">30 years</option>
-                    <option value="35">35 years</option>
-                </select>
             </div>
 
             <button type="button" class="one-flat-fee-package-calculator-submit-button">Calculate</button>
@@ -57,7 +36,11 @@ function one_flat_fee_package_calculator() { ?>
             <div class="one-flat-fee-package-calculator-estimate"></div>
         </form>
 
-        <p class="disclaimer">Based on a fully amortized fixed rate loan. Ask your agent for the tax rates in your area. Insurance estimate is based on an average cost, your final premium cost will be determined by the type of coverage you select. This program only provides an estimate.</p>
+        <p class="disclaimer">
+            *Based on buyers commission of 3.22% on first $100k/1.15% on balance<br>
+            ** Traditional Agent total cost is based on: 7% on first $100k/2.5% on Balance (incl buyers agent commissions*)<br>
+            *** ONEFLATFEE total cost is based from $3,999 plus buyers agent commissions*
+        </p>
     </div>
 
     <?php return ob_get_clean();
